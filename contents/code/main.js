dfb36ffb1cfbb6d5e3e7bf5c7a59622ec80d6319
Rafał Cisek <rafal.cisek121@gmail.com>
2026-04-29 00:53:17 +0200

    feat: Unmaximize to tile feature

---
+++ contents/code/main.js
@@ -0,0 +1,46 @@
+// Logging helpers
+const gLog_enabled = false;
+
+function gLog(section, object) {
+  if (gLog_enabled) {
+    console.log("[" + section + "]", JSON.stringify(object));
+  }
+}
+
+// Hooks registration
+for (const window of workspace.stackingOrder) {
+  registerUnmaximizeToTileHooks(window);
+}
+
+workspace.windowAdded.connect((window) => {
+  registerUnmaximizeToTileHooks(window);
+});
+
+// Unmaximize to tile feature
+
+function registerUnmaximizeToTileHooks(window) {
+  window.maximizedAboutToChange.connect((futureMaximizeMode) =>
+    aboutToMaximizeHook(window, futureMaximizeMode),
+  );
+  window.maximizedChanged.connect(() => maximizeHook(window));
+}
+
+const temporaryWindowTiles = {};
+
+function aboutToMaximizeHook(window, futureMaximizeMode) {
+  if (window.maximizeMode === 0) {
+    temporaryWindowTiles[window] = window.tile;
+  }
+  if (window.maximizeMode !== 0) {
+    window.tile = tile;
+  }
+  gLog("aboutToMaximizeHookRan", window);
+}
+
+function maximizeHook(window) {
+  const tile = temporaryWindowTiles[window];
+  if (window.maximizeMode === 0) {
+    window.tile = tile;
+  }
+  gLog("maximizeHookRan", window);
+}
